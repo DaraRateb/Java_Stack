6e5fb25d533bfb59e38985fbbbec2a699f20f914
Dara <dararateb93@gmail.com>
2021-06-12 19:59:43 +0300

    Java Assignments

---
+++ Java OOP/object_master/HumanTest.java
@@ -0,0 +1,9 @@
+   
+   public class HumanTest{
+   public static void main (String [] args){
+        Human Muhammad = new Human();
+        Human Laila = new Human();
+        Laila.attack(Muhammad);
+        System.out.println( Muhammad.health);
+    }
+   }
